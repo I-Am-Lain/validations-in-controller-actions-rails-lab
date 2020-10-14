ea21c6b6494b08e0887786ca87bb30ff679e6f25
I-Am-Lain <vetter.william@gmail.com>
2020-10-14 11:55:02 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,15 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    set_post!
+    @test_post = Post.new(post_params)
 
-    redirect_to post_path(@post)
+    if @test_post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
